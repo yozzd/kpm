66059cbb49551029b0d55f50b0c9c0585e9b40cd
yozzd <yozzdharma@gmail.com>
2015-08-05 08:55:43 +0700

    update #14

--- client/app/rekam/daftar/daftar.controller.js
+++ client/app/rekam/daftar/daftar.controller.js
@@ -19,6 +19,14 @@ angular.module('kpmApp')
 
         $scope.gridOptions.columnDefs = [
             {
+                name: 'index',
+                displayName: 'No.',
+                enableColumnMenu: false,
+                enableFiltering: false,
+                pinnedLeft: true,
+                cellTemplate: 'app/rekam/daftar/template/index.html',
+                width: 50
+            }, {
                 name: 'nomor',
                 displayName: 'Nomor',
                 enableColumnMenu: false,
@@ -28,8 +36,7 @@ angular.module('kpmApp')
                     priority: 0,
                 },
                 width: 100
-            },
-            {
+            }, {
                 name: 'tanggal',
                 displayName: 'Tanggal',
                 enableColumnMenu: false,
@@ -37,102 +44,86 @@ angular.module('kpmApp')
                 pinnedLeft: true,
                 cellTemplate: 'app/rekam/daftar/template/tanggal.html',
                 width: 150
-            },
-            {
+            }, {
                 name: 'nama',
                 displayName: 'Nama',
                 enableColumnMenu: false,
                 pinnedLeft: true,
                 cellTemplate: 'app/rekam/daftar/template/nama.html',
                 width: 150
-            },
-            {
+            }, {
                 name: 'umur',
                 displayName: 'Umur',
                 enableColumnMenu: false,
                 width: 100
-            },
-            {
+            }, {
                 name: 'jeniskelamin',
                 displayName: 'Jenis Kelamin',
                 enableColumnMenu: false,
                 width: 100
-            },
-            {
+            }, {
                 name: 'jalan',
                 displayName: 'Jalan',
                 enableColumnMenu: false,
                 width: 200
-            },
-            {
+            }, {
                 name: 'lingkungan',
                 displayName: 'Lingkungan',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'kelkec',
                 displayName: 'Kelurahan / Kecamatan',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'kotkab',
                 displayName: 'Kota / Kabupaten',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'provinsi',
                 displayName: 'Provinsi',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'telp',
                 displayName: 'No. Telp. / HP',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'suku',
                 displayName: 'Suku',
                 enableColumnMenu: false,
                 width: 100
-            },
-            {
+            }, {
                 name: 'agama',
                 displayName: 'Agama',
                 enableColumnMenu: false,
                 width: 100
-            },
-            {
+            }, {
                 name: 'pekerjaan',
                 displayName: 'Pekerjaan',
                 enableColumnMenu: false,
                 width: 150
-            },
-            {
+            }, {
                 name: 'statuskeluarga',
                 displayName: 'Status Keluarga',
                 enableColumnMenu: false,
                 width: 100
-            },
-            {
+            }, {
                 name: 'dikirim',
                 displayName: 'Dikirim / Konsul dari',
                 enableColumnMenu: false,
                 cellTemplate: 'app/rekam/daftar/template/dikirim.html',
                 width: 250
-            },
-            {
+            }, {
                 name: 'pembiayaan',
                 displayName: 'Pembiayaan',
                 enableColumnMenu: false,
                 cellTemplate: 'app/rekam/daftar/template/pembiayaan.html',
                 width: 250
-            },
-            {
+            }, {
                 name: 'timestamp',
                 displayName: '',
                 enableColumnMenu: false,
@@ -140,14 +131,6 @@ angular.module('kpmApp')
                 enableFiltering: false,
                 cellTemplate: 'app/rekam/daftar/template/timestamp.html',
                 width: 350,
-            },
-            {
-                name: 'edit',
-                displayName: 'Edit',
-                enableColumnMenu: false,
-                enableFiltering: false,
-                cellTemplate: '<button type="button" ng-click="grid.appScope.edit(row.entity._id)" class="ui-grid-cell-contents">Edit</button>',
-                width: 100,
             }
         ];
 
