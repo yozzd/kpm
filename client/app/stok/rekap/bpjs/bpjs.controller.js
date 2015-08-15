ca7c70e8e40ce95902adfb738ce01388919f2194
yozzd <yozzdharma@gmail.com>
2015-08-15 15:33:11 +0700

    update #41

--- client/app/stok/rekap/bpjs/bpjs.controller.js
+++ client/app/stok/rekap/bpjs/bpjs.controller.js
@@ -14,11 +14,11 @@ angular.module('kpmApp')
                     return value.lists.length > 0 && value._pasien.pembiayaan === 'BPJS';
                 });
 
-                var temp = [];
+                var temp1 = [];
                 _.forEach(filter1, function (val1) {
                     _.forEach(val1.lists, function (val2) {
                         _.forEach(val2.items, function (val3) {
-                            temp.push({
+                            temp1.push({
                                 tanggal: val2.tanggal,
                                 bulan: val2.bulan,
                                 tahun: val2.tahun,
@@ -30,35 +30,77 @@ angular.module('kpmApp')
                     });
                 });
 
-                var filter2 = _.filter(temp, function (value) {
+                var filter2 = _.filter(temp1, function (value) {
                     return value.bulan === b.toString() && value.tahun === t.toString();
                 });
                 $scope.bydate = _.chain(filter2).uniq('tanggal').pluck('tanggal').sortBy().value();
                 $scope.obat = _.chain(filter2).uniq('obat').pluck('obat').sortBy().value();
 
                 var temp2 = [];
+                var jumlah = [];
                 _.forEach($scope.obat, function (val1) {
                     var filter3 = _.filter(filter2, function (val2) {
                         return val2.obat === val1;
                     });
-                    temp2.push({
-                        items: filter3
+                    var sum = 0;
+                    _.forEach(filter3, function (val2) {
+                        sum += val2.jumlah;
+                    });
+                    jumlah.push(sum);
+                    temp2.push(filter3);
+                });
+
+                var temp3 = [];
+                _.forEach($scope.bydate, function (val1) {
+                    _.forEach(temp2, function (val2) {
+                        var filter4 = _.findWhere(val2, {
+                            tanggal: val1
+                        });
+                        temp3.push(filter4);
                     });
                 });
 
-                $scope.temp3 = [];
-                _.forEach($scope.bydate, function (val1) {
-                    _.forEach(temp2, function (val2) {
-                        var filter4 = _.findWhere(val2.items, {
-                            tanggal: val1
-                        });
-                        $scope.temp3.push(filter4);
+                var temp4 = [];
+                _.forEach(temp3, function (val) {
+                    if (val === undefined) {
+                        temp4.push(0);
+                    } else {
+                        temp4.push(val.jumlah);
+                    }
+                });
+
+                $scope.total = 0;
+                _.forEach(jumlah, function (val) {
+                    $scope.total += val;
+                });
+
+                //column partition
+                function partition1(arr, divider) {
+                    var group = _.groupBy(arr, function (item, i) {
+                        return Math.floor(i % divider);
                     });
+                    return _.map(group);
+                }
+                //row partition
+                function partition2(arr, divider) {
+                    var group = _.groupBy(arr, function (item, i) {
+                        return Math.floor(i / divider);
+                    });
+                    return _.map(group);
+                }
+
+                $scope.jumlah = partition1(jumlah, $scope.obat.length);
+                $scope.column = partition1(temp4, $scope.obat.length);
+
+                var crow = partition2(temp4, $scope.obat.length);
+                var temp5 = [];
+                _.forEach(crow, function (val, key) {
+                    var count = _.reduce(crow[key], function (a, b) {
+                        return a + b;
+                    });
+                    temp5.push(count);
                 });
-                $scope.cut = _.groupBy(_.flatten($scope.temp3), function (item, i) {
-                    return Math.floor(i % $scope.obat.length);
-                });
-                $scope.result = _.map($scope.cut);
+                $scope.row = partition1(temp5, $scope.bydate.length);
 
                 socket.syncUpdates('resep', $scope.datas);
             });
